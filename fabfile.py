59636ce2a2270f7253533783038ca272d87c19d3
Matt Stiles <mstiles@Matts-MacBook-Pro.local>
2013-11-12 15:39:39 -0500

    Download copy doc as an xls file

--- fabfile.py
+++ fabfile.py
@@ -111,9 +111,9 @@ def download_copy():
     """
     Downloads a Google Doc as an .xls file.
     """
-    base_url = 'https://docs.google.com/spreadsheet/pub?key=%s&output=csv'
+    base_url = 'https://docs.google.com/spreadsheet/pub?key=%s&output=xls'
     doc_url = base_url % app_config.COPY_GOOGLE_DOC_KEY
-    local('curl -o data/copy.csv "%s"' % doc_url)
+    local('curl -o data/copy.xls "%s"' % doc_url)
 
 def update_copy():
     """
@@ -123,7 +123,7 @@ def update_copy():
 
 def download_data():
     """
-    Downloads a Google Spreadsheet as an .xls file.
+    Downloads a Google Spreadsheet as an .csv file.
     """
     base_url = 'https://docs.google.com/spreadsheet/pub?key=%s&output=csv'
     doc_url = base_url % app_config.COPY_GOOGLE_SPREADSHEET_KEY

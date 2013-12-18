7235585de9b033b65b8d6d52dfd190701dc173a9
Danny DeBelius <danny.debelius@gmail.com>
2013-12-18 16:42:03 -0500

    Serving hosted fonts again

--- www/js/fonts.js
+++ www/js/fonts.js
@@ -1,7 +1,7 @@
 WebFont.load({
 	custom: {
-		families: ['Gotham SSm:n4,n7'],
-		urls: ['font/GothamSSm.css']
+		families: ['Gotham SSm:n4,n7', 'Gotham:n4,n7'],
+		urls: ['font/GothamSSm.css', 'font/Gotham.css']
 	},
 	timeout: 10000
 });

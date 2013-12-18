88b48383ce9724deabe2c3cbb5c026a4c8323a57
Danny DeBelius <danny.debelius@gmail.com>
2013-12-18 17:43:14 -0500

    Light weight gotham

--- www/js/fonts.js
+++ www/js/fonts.js
@@ -1,7 +1,7 @@
 WebFont.load({
 	custom: {
-		families: ['Gotham SSm:n4,n7', 'Gotham:n4,n7'],
-		urls: ['font/GothamSSm.css', 'font/Gotham.css']
+		families: ['Gotham SSm A:n3,n4,n8', 'Gotham SSm B:n3,n4,n8', 'Gotham A:n3,n4,n7','Gotham B:n3,n4,n7'],
+		urls: ['font/gotham_hfj.css']
 	},
 	timeout: 10000
 });

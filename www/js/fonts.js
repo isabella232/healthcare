847116bb4ad3d5a4010a97c4b07e285c5bc4c046
Danny DeBelius <danny.debelius@gmail.com>
2013-12-19 10:49:25 -0500

    Text set in our old friend helvetica

--- www/js/fonts.js
+++ www/js/fonts.js
@@ -1,7 +1,7 @@
 WebFont.load({
 	custom: {
-		families: ['Gotham SSm A:n3,n4,n8', 'Gotham SSm B:n3,n4,n8', 'Gotham A:n3,n4,n7','Gotham B:n3,n4,n7'],
-		urls: ['font/gotham_hfj.css']
+		families: ['Gotham:n4,n7', 'Gotham SSm:n4,n7'],
+		urls: ['font/Gotham.css', 'font/GothamSSm.css']
 	},
 	timeout: 10000
 });
